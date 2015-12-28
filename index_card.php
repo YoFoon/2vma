35c2803013d16c362511295d1da8b921aa562434
YoFoon <mayuefeng@123feng.com>
2015-12-28 08:20:33 +0800

    移动端

--- index_card.php
+++ index_card.php
@@ -1,8 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta http-equiv="Cache-Control" content="no-transform">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1,user-scalable=no">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta http-equiv="Pragma" content="no-cache">
+<meta name="robots" content="nofollow,noindex,noarchive">
+<meta http-equiv="Cache-Control" content="no-cache">
+<meta http-equiv="Expires" content="0">
 <meta name="generator" content="二维码">
 <meta name="author" content="YoFoon">
 <meta name="description" content="二维码">
